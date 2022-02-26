da9ce2b98205a0148ba69b65eed364c3daef3f52
David Gaspar <davidgaspar.dev@gmail.com>
2022-02-26 18:07:27 +0000

    Creating functions for routes

--- src/server/server.go
+++ src/server/server.go
@@ -9,17 +9,22 @@ func RunServer() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
 		switch request.URL.Path {
 		case PATH_API_CREATE_RECIVE:
-			if isRequestGetApplicationJson(request) {
-				responseOK(response, map[string]string{
-					"hello": "World!",
-				})
-			} else {
-				responseBadRequest(response, fmt.Errorf("Request isn't GET for application/json"))
-			}
+			routePathApiCreateRecive(response, request)
 		default:
 			http.NotFound(response, request)
 		}
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func routePathApiCreateRecive(response http.ResponseWriter, request *http.Request) {
+	if !isRequestGetApplicationJson(request) {
+		responseBadRequest(response, fmt.Errorf("Request isn't GET for application/json"))
+		return
+	}
+
+	responseOK(response, map[string]string{
+		"hello": "World!",
+	})
 }
