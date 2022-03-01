51405c3c10b953bf64ee68539f3b292af8e453d9
David Gaspar <davidgaspar.dev@gmail.com>
2022-03-01 13:17:03 +0000

    Validation for request with POST method (application/json)

--- src/server/validation.go
+++ src/server/validation.go
@@ -2,8 +2,16 @@ package server
 
 import "net/http"
 
+func isRequestPostApplicationJson(req *http.Request) bool {
+	return isRequestPost(req) && isRequestApplicationJson(req)
+}
+
 func isRequestGetApplicationJson(req *http.Request) bool {
 	return isRequestGet(req) && isRequestAcceptApplicationJson(req)
+}
+
+func isRequestPost(req *http.Request) bool {
+	return req.Method == METHOD_POST
 }
 
 func isRequestGet(req *http.Request) bool {
